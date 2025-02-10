167b697f9b36e07bc8d3b2bba5827d1e4b85f568
kamegoro <tentyo0620@icloud.com>
2025-02-10 21:59:48 +0900

    update infrastructure

---
+++ src/infrastructure/csv.py
@@ -0,0 +1,12 @@
+import polars as pl
+from port.csv import CsvPort
+
+class Csv(CsvPort):
+    def __init__(self, file_path: str):
+        self.file_path = file_path
+
+    def read_csv(self) -> pl.DataFrame:
+        return pl.read_csv(self.file_path)
+
+    def write_csv(self, df: pl.DataFrame, file_path: str) -> None:
+        return df.write_csv(file_path)
