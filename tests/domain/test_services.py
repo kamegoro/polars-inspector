108183087365df7a44b3c2a970308aa999d69b58
kamegoro <tentyo0620@icloud.com>
2025-02-10 22:24:06 +0900

    update domain test mock

--- tests/domain/test_services.py
+++ tests/domain/test_services.py
@@ -2,14 +2,14 @@ import polars as pl
 from domain.models import Dataframe
 from domain.services import CleanDataframe
 from infrastructure.csv import Csv
-from unittest.mock import patch
+from unittest.mock import Mock
 
 
-@patch.object(Csv, "read_csv")
-def test_小文字の名前を大文字にする(mock_read_csv):
-    mock_read_csv.return_value = pl.DataFrame({"name": ["alice", "bob"]})
+def test_小文字の名前を大文字にする():
+    mock_csv = Mock(spec=Csv)
+    mock_csv.read_csv.return_value = pl.DataFrame({"name": ["alice", "bob"]})
 
-    clean_dataframe = CleanDataframe(Csv("test.csv"))
+    clean_dataframe = CleanDataframe(mock_csv)
     actual = clean_dataframe.exec()
 
     expected = Dataframe({"name": ["ALICE", "BOB"]})  # 手動入力の期待値
@@ -17,11 +17,11 @@ def test_小文字の名前を大文字にする(mock_read_csv):
     assert actual.equals(expected)
 
 
-@patch.object(Csv, "read_csv")
-def test_空白を削除する(mock_read_csv):
-    mock_read_csv.return_value = pl.DataFrame({"name": [" ALICE", "BOB "]})
+def test_空白を削除する():
+    mock_csv = Mock(spec=Csv)
+    mock_csv.read_csv.return_value = pl.DataFrame({"name": [" ALICE", "BOB "]})
 
-    clean_dataframe = CleanDataframe(Csv("test.csv"))
+    clean_dataframe = CleanDataframe(mock_csv)
     actual = clean_dataframe.exec()
 
     expected = pl.DataFrame({"name": ["ALICE", "BOB"]})
@@ -29,11 +29,11 @@ def test_空白を削除する(mock_read_csv):
     assert actual.equals(expected)
 
 
-@patch.object(Csv, "read_csv")
-def test_nullをUnknownに変換する(mock_read_csv):
-    mock_read_csv.return_value = pl.DataFrame({"name": [None, "BOB"]})
+def test_nullをUnknownに変換する():
+    mock_csv = Mock(spec=Csv)
+    mock_csv.read_csv.return_value = pl.DataFrame({"name": [None, "BOB"]})
 
-    clean_dataframe = CleanDataframe(Csv("test.csv"))
+    clean_dataframe = CleanDataframe(mock_csv)
     actual = clean_dataframe.exec()
 
     expected = pl.DataFrame({"name": ["Unknown", "BOB"]})
