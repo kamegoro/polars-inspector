dcb1135b4810b65981b336fb9e7d7aefcfa06c3c
kamegoro <tentyo0620@icloud.com>
2025-02-10 22:25:38 +0900

    update format

---
+++ src/main.py
@@ -0,0 +1,11 @@
+from infrastructure.csv import Csv
+from domain.services import CleanDataframe
+from usecase.precess_data import Executer
+
+
+def exec():
+    csvInfrastructure = Csv("target.csv")
+    cleanDataframeDomain = CleanDataframe(csvInfrastructure)
+    cleanExecuter = Executer(cleanDataframeDomain)
+
+    cleanExecuter.process()
