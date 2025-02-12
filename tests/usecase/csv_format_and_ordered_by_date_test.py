55344ecf653cf7045c5e20cc1d8aa4674bdb0797
kamegoro <tentyo0620@icloud.com>
2025-02-12 20:12:47 +0900

    update usecase

--- tests/usecase/csv_format_and_ordered_by_date_test.py
+++ tests/usecase/csv_format_and_ordered_by_date_test.py
@@ -1,29 +1,27 @@
-from unittest.mock import Mock, patch
+from unittest.mock import patch
 from usecase.csv_format_and_ordered_by_date import csvFormatAndOrderedByDate
-from port.csv import CsvPort
-from domain.models import Dataframe
-from domain.services import CleanDataframe
+from domain.dataframe import Dataframe
 
 
-@patch("usecase.csv_format_and_ordered_by_date.pl.read_csv")
-def test_CSVを読み込んで整形されて日付が降順に並び替えられる(mock_read_csv):
-    mock_csv_port = Mock(spec=CsvPort)
+@patch("domain.dataframe.Dataframe.write")
+@patch("domain.dataframe.Dataframe.sortByDate")
+@patch("domain.dataframe.Dataframe.clean")
+@patch("domain.dataframe.Dataframe.read")
+def test_CSVを読み込んで整形されて日付が降順に並び替えられる(mock_read, mock_clean, mock_sortByDate, mock_write):
+    base_dataframe = Dataframe({"name": [" aLICE", "Bob"], "date": ["2021-01-01", "2021-01-02"]})
+    cleaned_dataframe = Dataframe({"name": ["ALICE", "BOB"], "date": ["2021-01-01", "2021-01-02"]})
+    ordered_dataframe = Dataframe({"name": ["ALICE", "BOB"], "date": ["2021-01-02", "2021-01-01"]})
 
-    base_dataframe = Dataframe({"name": [" aLICE", "Bob"]})
+    mock_read.return_value = base_dataframe
+    mock_clean.return_value = cleaned_dataframe
+    mock_sortByDate.return_value = ordered_dataframe
+    mock_write.return_value = None
 
-    # csvを読み込んでDataframeを返す
-    mock_read_csv.return_value = base_dataframe
-    mock_clean_dataframe = Mock(spec=CleanDataframe)
-
-    # dataframeをクリーンする
-    mock_clean_dataframe.exec.return_value = Dataframe({"name": ["ALICE", "BOB"]})
-
-    # dataframeの日付順に並び替える
-    # dataframeをcsvとして吐き出す
-
-    actual = csvFormatAndOrderedByDate(mock_csv_port)
+    actual = csvFormatAndOrderedByDate()
     expected = None
 
     assert actual is expected
-    mock_read_csv.assert_called_once_with("target.csv")
-    mock_clean_dataframe.exec.assert_called_once_with(base_dataframe)
+    mock_read.assert_called_once_with("../target.csv")
+    mock_clean.assert_called_once_with(base_dataframe)
+    mock_sortByDate.assert_called_once_with(cleaned_dataframe)
+    mock_write.assert_called_once_with(ordered_dataframe, "../result.csv")
